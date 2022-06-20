6fe2a7da998da514961a6d1bc5a5e81a8a672cd9
Jakub Zięba <zieba.jakub.97@gmail.com>
2022-06-20 23:51:58 +0200

    ADD Expense Repository

---
+++ BudgetAPI/Repositories/ExpenseRepository.cs
@@ -0,0 +1,21 @@
+﻿using BudgetAPI.Data.Configuration;
+using BudgetAPI.Models;
+
+namespace BudgetAPI.Repositories
+{
+    public interface IExpenseRepository
+    {
+        Task InsertNewExpense(Expense expense, CancellationToken cancellationToken);
+    }
+    internal class SQLExpenseRepository : IExpenseRepository
+    {
+        public async Task InsertNewExpense(Expense expense, CancellationToken cancellationToken)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                await context.AddAsync(expense, cancellationToken);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}

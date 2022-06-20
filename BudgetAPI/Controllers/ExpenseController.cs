15a92345de8dfc40d1db8de786209c5c92cbacb6
Jakub Zięba <zieba.jakub.97@gmail.com>
2022-06-20 23:24:30 +0200

    ADD Expense Controller, REMOVED unused default controller

---
+++ BudgetAPI/Controllers/ExpenseController.cs
@@ -0,0 +1,30 @@
+﻿using BudgetAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BudgetAPI.Controllers
+{
+    public class ExpenseController : Controller
+    {
+        private readonly IExpenseService expenseService;
+
+        public ExpenseController(IExpenseService expenseService)
+        {
+            this.expenseService = expenseService ?? throw new ArgumentNullException(nameof(expenseService));
+        }
+
+        [HttpPost]
+        [Route("CreateExpense", Name = nameof(CreateExpense))]
+        public async Task<ActionResult> CreateExpense(decimal amount, string userId, string desc, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var expense = await expenseService.CreateExpense(amount, userId, desc, cancellationToken);
+                return Ok(expense);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
+    }
+}

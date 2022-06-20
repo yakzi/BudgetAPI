cc287c3cebb36ffbf7600727da90b819138f1952
Jakub Zięba <zieba.jakub.97@gmail.com>
2022-06-20 22:40:14 +0200

    ADD DbContext

---
+++ BudgetAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using BudgetAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BudgetAPI.Data.Configuration
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Expense> Expenses { get; set; }
+        public DbSet<Income> Incomes { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ExpensesTest;");
+        }
+    }
+}

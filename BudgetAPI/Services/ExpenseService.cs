0677a49f07786a6ca55202d413ce4e0195711a8a
Jakub Zięba <zieba.jakub.97@gmail.com>
2022-06-20 23:23:47 +0200

    ADD Expense Service

---
+++ BudgetAPI/Services/ExpenseService.cs
@@ -0,0 +1,26 @@
+﻿using BudgetAPI.Data.Configuration;
+using BudgetAPI.Models;
+
+namespace BudgetAPI.Services
+{
+
+    public interface IExpenseService
+    {
+        Task<Expense> CreateExpense(decimal amount, string userId, string desc, CancellationToken cancellationToken);
+        //Task<List<Expense>> GetExpensesForUser(string userId, CancellationToken cancellationToken);
+
+    }
+    internal class ExpenseService : IExpenseService
+    {
+        public async Task<Expense> CreateExpense(decimal amount, string userId, string desc, CancellationToken cancellationToken)
+        {
+            var Expense = new Expense(amount, Guid.Parse(userId), desc);
+            using (var context = new ApplicationDbContext())
+            {
+               await context.AddAsync(Expense, cancellationToken);
+               await context.SaveChangesAsync();
+            }
+            return Expense;
+        }
+    }
+}

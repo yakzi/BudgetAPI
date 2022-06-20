beec358e58ca70c120fd6481b4c7072274e1f303
Jakub Zięba <zieba.jakub.97@gmail.com>
2022-06-20 22:40:33 +0200

    ADD UserService interface + class

---
+++ BudgetAPI/Services/UserService.cs
@@ -0,0 +1,24 @@
+﻿using BudgetAPI.Data.Configuration;
+using BudgetAPI.Models;
+
+namespace BudgetApi.Services
+{
+
+    public interface IUserService
+    {
+        Task<User> CreateUser(string email, string password, string name, CancellationToken cancellationToken);
+    }
+    internal class UserService : IUserService
+    {
+        public async Task<User> CreateUser(string email, string password, string name, CancellationToken cancellationToken)
+        {
+            var User = new User(email, password, name);
+            using (var context = new ApplicationDbContext())
+            {
+               await context.AddAsync(User, cancellationToken);
+               await context.SaveChangesAsync();
+            }
+            return User;
+        }
+    }
+}

b162026bb4695b7e360e9f355fa3261fe8c6f959
Jakub Zięba <zieba.jakub.97@gmail.com>
2022-06-20 23:05:57 +0200

    ADD UserController

---
+++ BudgetAPI/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using BudgetApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BudgetAPI.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IUserService userService;
+
+        public UserController(IUserService userService)
+        {
+            this.userService = userService ?? throw new ArgumentNullException(nameof(userService));
+        }
+
+        [HttpPost]
+        [Route("CreateUser", Name = nameof(CreateUser))]
+        public async Task<ActionResult> CreateUser(string email, string password, string name, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var user = await userService.CreateUser(email, password, name, cancellationToken);
+                return Ok(user);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
+    }
+}

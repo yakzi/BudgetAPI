91ae4ef71e76353ed1215624489cd01ce03ded84
Jakub Zięba <zieba.jakub.97@gmail.com>
2022-06-20 23:45:16 +0200

    ADD Repository to User

---
+++ BudgetAPI/Repositories/UserRepository.cs
@@ -0,0 +1,21 @@
+﻿using BudgetAPI.Data.Configuration;
+using BudgetAPI.Models;
+
+namespace BudgetAPI.Repositories
+{
+    public interface IUserRepository
+    {
+        Task InsertNewUser(User user, CancellationToken cancellationToken);
+    }
+    internal class SQLUserRepository : IUserRepository
+    {
+        public async Task InsertNewUser(User user, CancellationToken cancellationToken)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                await context.AddAsync(user, cancellationToken);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
